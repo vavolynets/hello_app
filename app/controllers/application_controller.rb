c29c17e479f5078b8da501b597cdc88ef4c93923
Valentyn A. Volynets <vavolynets@gmail.com>
2017-04-24 13:47:00 +0000

    Change root page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   def empty
     uip = request.remote_ip
     uap = request.user_agent
-    render html: "Hello,\r\nyour IP: [#{uip}]\r\nyour UA: (#{uap})\r\n"
+    render html: "Hello,\r\n\r\nyour IP: [#{uip}]\r\n\r\nyour UA: (#{uap})\r\n\r\n"
   end
 end
